26aabc214341c9debbe9ec275d8946f1fd3ee3e5
Mo2024 <mohdosama2025@gmail.com>
2024-11-16 19:19:36 +0300

    added springboot

---
+++ backend/src/test/java/com/artisan/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artisan.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
