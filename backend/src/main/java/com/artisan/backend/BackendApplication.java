c7ec0eb7b751ddb72ae61307fc75ad7c13a099cb
Mo2024 <mohdosama2025@gmail.com>
2025-03-14 18:21:28 +0300

    edit journal voucher works

--- backend/src/main/java/com/artisan/backend/BackendApplication.java
+++ backend/src/main/java/com/artisan/backend/BackendApplication.java
@@ -1,9 +1,8 @@
 package com.artisan.backend;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.env.Environment;
+
 
 @SpringBootApplication
 public class BackendApplication {
